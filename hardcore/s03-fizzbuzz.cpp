426f26b5b6288ef4ffa01132a6bf600bc072730c
PJATKMikolaj <92036588+PJATKMikolaj@users.noreply.github.com>
2021-11-09 22:18:09 +0100

    Create s03-fizzbuzz.cpp

---
+++ hardcore/s03-fizzbuzz.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int n = std::atoi(argv[1]); 
+	
+	while(n>=1) {
+		
+		std::cout << n << std::endl;
+		if (n%3==0 && n%5==0)
+			std::cout << "FizzBuzz"<< std::endl;
+		else if (n%3==0)
+			std::cout << "Fizz"<< std::endl;
+		else if (n%5==0)
+			std::cout << "Buzz"<< std::endl;
+		
+		n--;
+	}
+	
+	return 0;
+}
