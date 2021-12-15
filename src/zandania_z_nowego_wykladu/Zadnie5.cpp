654e5b9c108e18ae70a28bd9665cb43ee2c76ce6
MateuszOriol <94061760+MateuszOriol@users.noreply.github.com>
2021-12-15 17:48:22 +0100

    Create Zadnie5.cpp

---
+++ src/zandania_z_nowego_wykladu/Zadnie5.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <iostream>
+
+void is_even(int a)
+{
+	if (a % 2 == 0)
+		std::cout << "jest parzysta";
+	else
+		std::cout << "jest nieparzysta";
+}
+int main()
+{
+	int a;
+	std::cout << "podaj liczbe" << std::endl;
+	std::cin >> a;
+	is_even(a);
+	return 0;
+}
