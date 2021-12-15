25cac11546f99b390b671912e997079a21b1c44e
MateuszOriol <94061760+MateuszOriol@users.noreply.github.com>
2021-12-15 16:21:35 +0100

    Create Zadanie2.cpp

---
+++ src/zandania_z_nowego_wykladu/Zadanie2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string imie = "Mateusz";
+	double waga = 63;
+	int wzrost = 188;
+
+	std::cout << "Imie: " << imie << std::endl << "Waga: " << waga<<" kg" << std::endl << "Wzrost: " << wzrost << " cm" << "\n";
+	system("pause");
+    return 0;
+}
