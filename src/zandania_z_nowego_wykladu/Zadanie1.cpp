cab3fa50f072031d47c3cf2fa68d56ed48f9566c
MateuszOriol <94061760+MateuszOriol@users.noreply.github.com>
2022-01-12 16:58:56 +0100

    Update Zadanie1.cpp

--- src/zandania_z_nowego_wykladu/Zadanie1.cpp
+++ src/zandania_z_nowego_wykladu/Zadanie1.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+
+using namespace std;
+
+bool l_pierwsza(int a)
+{
+    int licznik=0;
+    for(int i=2;i<a;i++)
+    {
+    if(a%i==0)
+    licznik++;
+    }
+    if(licznik==0)
+        return true;
+    else
+        return false;
+}
 
 int main()
 {
-	std::string x = "2 3 5 7 11 13 17 19";
-	std::cout << x << std::endl << x << std::endl;
+    int w;
+    string pierwsze,liczba;
+    for(int i=2;i<21;i++)
+    {
+    w=l_pierwsza(i);
+    if(w==1)
+    {
+    stringstream uno;
+    uno << i;
+    string strus = uno.str();
+    pierwsze = pierwsze + strus + " ";
+    }
+    }
+    cout<<pierwsze<<endl;
+    cout<<pierwsze<<endl;
 	system("pause");
-    return 0;
+return 0;
 }
